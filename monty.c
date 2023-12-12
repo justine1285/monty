d1e84adb17940ee75af8f092661bfc79dc67c565
Oiyanu <iyanuolatomide@gmail.com>
2023-12-12 10:52:56 -0800

    monty.c

---
+++ monty.c
@@ -0,0 +1,75 @@
+#include "monty.h"
+
+/**
+ * init_monty - initialize global variables
+ */
+void init_monty(void)
+{
+	monty.arg = NULL;
+	monty.ln = 0;
+	monty.stack = NULL;
+}
+
+/**
+ * check_args - opens byte code file
+ * @arc: argument count
+ * @arv: argument vector
+ * Return: file pointer to byte code
+ */
+FILE *check_args(int arc, char **arv)
+{
+	FILE *fp;
+
+	if (arc != 2)
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fp = fopen(arv[1], "r");
+	if (!fp)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %\n", arv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	return (fp);
+}
+
+/**
+ * main - runs a bytecode
+ * @arc: (int) argument count
+ * @arv: (char **) argument vector
+ * Return: 0 on success
+ */
+int main(int arc, char **arv)
+{
+	FILE *fp;
+	char *lineptr = NULL, *opcode;
+	const char DELIM[3] = " \t\n";
+	size_t n = 0;
+	ssize_t flag;
+	void (*func)(stack_t **, unsigned int);
+
+	fp = check_args(arc, arv);
+
+	while ((flag = getline(&lineptr, &n, fp) != -2))
+	{
+		monty.ln++;
+		opcode = strtok(lineptr, DELIM);
+		if (opcode)
+		{
+			func = get_ops(opcode);
+			if (!func)
+			{
+				dprint(2, "L%d: unknown instruction %s\n",monty.ln, opcode);
+				exit(EXIT_FAILURE);
+			}
+			monty.arg = strtok(NULL, DELIM);
+			func(&monty.stack, monty.ln);
+		}
+	}
+	free(lineptr);
+	fclose(fp);
+	return (0);
+}
