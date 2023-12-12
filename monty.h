49eac820ca84a138b7a4898083679c4aa80d348a
justine1285 <justineadugbo@gmail.com>
2023-12-12 03:22:11 -0800

    monty.h

--- monty.h
+++ monty.h
@@ -18,3 +18,55 @@
  * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function for stack, queues,
+ * LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct monty_s - global variable for monty interpreter
+ * @line_num: line number of byte code file
+ * @arg: opcode argument
+ * @stack: stack
+ */
+typedef struct monty_s
+{
+	unsigned int ln;
+	char *arg;
+	stack_t *stack;
+} monty_t;
+
+/* in monty.c */
+FILE *check_args(int, char **);
+void (*get_ops(char *))(stack_t **, unsigned int);
+void init_monty(void);
+
+/* in utility.c */
+int _isidigit(char *);
+
+extern monty_t monty;
+monty_t monty;
+
+/* operations */
+void push(stack_t **, unsigned int);
+void pall(stack_t **, unsigned int);
+void pint(stack_t **, unsigned int);
+void pop(stack_t **, unsigned int);
+void swap(stack_t **, unsigned int);
+
+#endif
